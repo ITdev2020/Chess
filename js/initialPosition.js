a0ddf7de29f1cea9893903a400b7310dc142dea2
ITdev2020 <igoris.talaiko.darbui@gmail.com>
2020-12-09 01:32:40 +0200

    09-01

--- js/initialPosition.js
+++ js/initialPosition.js
@@ -1,26 +1,35 @@
-piRook = '<img src="assets/chess-rook.svg" alt="chess-rook.svg">';
-document.querySelector('.A8').innerHTML = piRook;
-document.querySelector('.H8').innerHTML = piRook;
+var blackColor = 0.2;
+var whiteColor = 0.8;
 
-piKnight = '<img src="assets/chess-knight.svg" alt="chess-knight.svg">';
-document.querySelector('.B8').innerHTML = piKnight;
-document.querySelector('.G8').innerHTML = piKnight;
+let pieces = (piecesColor) => {
+	piRook = '<img src="assets/chess-rook_outline.svg" alt="chess-rook.svg">';
+	document.querySelector(".A8").innerHTML = piRook;
+	document.querySelector(".A8>img").style.filter = "invert(" + piecesColor + ")";
+	document.querySelector(".H8").innerHTML = piRook;
+	document.querySelector(".H8>img").style.filter = "invert(" + piecesColor + ")";
 
-piBishop = '<img src="assets/chess-bishop.svg" alt="chess-bishop.svg">';
-document.querySelector('.C8').innerHTML = piBishop;
-document.querySelector('.F8').innerHTML = piBishop;
+	piKnight = '<img src="assets/chess-knight_outline.svg" alt="chess-knight.svg">';
+	document.querySelector(".B8").innerHTML = piKnight;
+	document.querySelector(".G8").innerHTML = piKnight;
 
+	piBishop = '<img src="assets/chess-bishop_outline.svg" alt="chess-bishop.svg">';
+	document.querySelector(".C8").innerHTML = piBishop;
+	document.querySelector(".F8").innerHTML = piBishop;
 
-piQueen = '<img src="assets/chess-queen.svg" alt="chess-queen.svg">';
-document.querySelector('.D8').innerHTML = piQueen;
+	piQueen = '<img src="assets/chess-queen_outline.svg" alt="chess-queen.svg">';
+	document.querySelector(".D8").innerHTML = piQueen;
 
-piKing = '<img src="assets/chess-king.svg" alt="chess-king.svg">';
-document.querySelector('.E8').innerHTML = piKing;
+	piKing = '<img src="assets/chess-king_outline.svg" alt="chess-king.svg">';
+	document.querySelector(".E8").innerHTML = piKing;
 
-var rr;
-for(i=0; i<8; i++){
-rr+=String.fromCharCode(65 + i);
-}
+	for (i = 0; i < 8; i++) {
+		let column = String.fromCharCode(65 + i);
+		let piPawn = '<img src="assets/chess-pawn_outline.svg" alt="chess-pawn.svg">';
+		let pawnIndex = "." + column + "7";
+		document.querySelector(pawnIndex).innerHTML = piPawn;
+	}
+};
 
-
-arr=document.querySelector
+// pieces(blackColor);
+pieces(whiteColor);
+// arr = document.querySelector;
